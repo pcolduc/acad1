04709ae3f2a29c67434c8567d83bcc576ffd203f
pcolduc <137030645+pcolduc@users.noreply.github.com>
2023-06-26 22:35:07 +0900

    Add files via upload

---
+++ src/Quiz/Quiz1.java
@@ -0,0 +1,23 @@
+package dya16;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+
+            String str1 = sc.nextLine();
+
+            String word[] = str1.split(" ");
+            System.out.println(word.length);
+
+            }
+        }
+
+
+
+
+    }
+
